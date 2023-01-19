407887afd4c59b21a8862d705a88810e39ea8bc5
lorenzo <lorenzotijerina@gmail.com>
2023-01-19 18:46:33 -0500

    ready for github pages deploy

--- src/scripts/mover.js
+++ src/scripts/mover.js
@@ -1,23 +1,2 @@
-class Mover {
-  constructor(position, velocity, radius, isBaby = false) {
-    this.position = position;
-    this.velocity = velocity;
-    this.radius = radius;
-    this.isBaby = isBaby;
-  }
-  update() {
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-  }
-  handleImpact(impactForce) {
-    // Implement bounce
-  }
-  getBounds() {
-    return {
-      x: this.position.x,
-      y: this.position.y,
-      r: this.radius,
-    };
-  }
-}
+class Mover {}
 export default Mover;
