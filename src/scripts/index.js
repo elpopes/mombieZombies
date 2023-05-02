5fe875cc114208e583e26e414e708d94badc8ae8
lorenzo <lorenzotijerina@gmail.com>
2023-05-02 17:04:03 -0400

    prevent refresh and ipad bounce

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,4 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   new GameView(canvas, ctx).start();
+
+  let touchStartY = 0;
+
+  window.addEventListener("touchstart", function (event) {
+    if (event.touches.length === 1) {
+      touchStartY = event.touches[0].clientY;
+    }
+  });
+
+  window.addEventListener("touchmove", function (event) {
+    event.preventDefault();
+  });
 });
